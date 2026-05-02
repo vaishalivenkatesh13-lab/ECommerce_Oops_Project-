ae043aba8e67f3608c4c8c7a15a7464b6722081b
Yazhini <yazhini8e@gmail.com>
2026-05-02 11:50:09 +0530

    Added Payment class

---
+++ Payment.h
@@ -0,0 +1,24 @@
+#ifndef PAYMENT_H
+#define PAYMENT_H
+
+#include <string>
+using namespace std;
+
+class Payment {
+
+private:
+    int paymentID;
+    string paymentMethod;
+    string paymentStatus;
+    double amount;
+
+public:
+    Payment();
+
+    Payment(int id, const string& method, double amount);
+
+    void processPayment();
+    void generateBill() const;
+};
+
+#endif
