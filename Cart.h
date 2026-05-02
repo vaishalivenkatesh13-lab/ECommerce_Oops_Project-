2ca64ac5a1010c6e57f37482ce09f99393526a8b
Yazhini <yazhini8e@gmail.com>
2026-05-02 11:38:47 +0530

    Added Cart.h

---
+++ Cart.h
@@ -0,0 +1,24 @@
+#ifndef CART_H
+#define CART_H
+
+#include <vector>
+#include "Product.h"
+using namespace std;
+
+class Cart {
+
+private:
+    vector<Product> items;
+    double totalAmount;
+
+public:
+    Cart();
+
+    void addItem(Product p);
+    void removeItem(int productID);
+    void displayCart() const;
+    double calculateTotal();
+    void clearCart();
+};
+
+#endif
